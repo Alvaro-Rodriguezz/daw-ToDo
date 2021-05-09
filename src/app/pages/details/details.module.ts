2ca8135315fbae4305ae1240fe7654bee657bd45
Alvaro <0alvarorm0@gmail.com>
2021-05-09 22:22:17 +0100

    Details styles WIP

--- src/app/pages/details/details.module.ts
+++ src/app/pages/details/details.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { DetailsRoutingModule } from './details-routing.module';
 import { DetailsComponent } from './details.component';
+import { MatSliderModule } from '@angular/material/slider';
 
 
 @NgModule({
   declarations: [DetailsComponent],
   imports: [
     CommonModule,
-    DetailsRoutingModule
+    DetailsRoutingModule,
+    MatSliderModule
   ]
 })
 export class DetailsModule { }
